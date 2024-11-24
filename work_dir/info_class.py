4a7fdc839b88c9951f2bfbc13e2de5a52e3e0d72
Daniel Bond <dbond@kse.org.ua>
2024-11-24 10:30:50 +0200

    working search & output added -output

--- work_dir/info_class.py
+++ work_dir/info_class.py
@@ -12,5 +12,17 @@ class Info:
         self.event13 = event13
         self.medal14 = medal14
 
+    def return_stat(self):
+        return f"{self.name1} - {self.sport12} - {self.medal14}"
+
+    def medal_to_num(self):
+        if self.medal14 == "Gold":
+            return 1
+        if self.medal14 == "Silver":
+            return 2
+        if self.medal14 == "Bronze":
+            return 3
+        return 0
+
     def __str__(self):
         return f"{self.id0}, {self.name1}, {self.team6}, {self.NOC7}, {self.games8}, {self.year9}, {self.season10}, {self.city11}, {self.sport12}, {self.event13}, {self.medal14}"
