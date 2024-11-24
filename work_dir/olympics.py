69153a04a970399207cd664d3f19aa9d0158d9c5
Daniel Bond <dbond@kse.org.ua>
2024-11-24 09:51:53 +0200

    argparse search added -medals saving found info

--- work_dir/olympics.py
+++ work_dir/olympics.py
@@ -1,20 +1,24 @@
 import argparse
 from info_class import Info
 
+#config: events.tsv -medals United States 1972
 parser = argparse.ArgumentParser()
 parser.add_argument("filename", help="Ask for input file")
-parser.add_argument("-medals", help="Ask for medals of team")
+parser.add_argument("-medals", help="Ask for medals of team", nargs="+")
+parser.add_argument("-output", help="Filename for output")
 
 args = parser.parse_args()
+print(args)
 
 all_info = []
 with open(args.filename, "r") as file:
     next(file)
     for line in file:
         s = line.split("\t")
-        check1 = f"{s[6]} {s[9]}"
-        check2 = f"{s[7]} {s[9]}"
-        if args.medals == check1  or args.medals == check2:
+        team_search = " ".join(args.medals[:-1])
+        year_search = args.medals[-1]
+
+        if (team_search in s[6]  or team_search in s[7]) and year_search == s[9]:
             all_info.append(Info(s[0],s[1],s[6],s[7],s[8],s[9],s[10],s[11],s[12],s[13],s[14]))
 
 print(*all_info)
